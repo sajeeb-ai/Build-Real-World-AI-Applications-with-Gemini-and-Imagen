02e7ca311022a56f32d06e30a3f26c58dc643a95
Sajeeb <122395122+sajeeb-ai@users.noreply.github.com>
2024-10-30 16:20:23 +0600

    Add files via upload

---
+++ SendChatwithoutStream.py
@@ -0,0 +1,32 @@
+import vertexai
+from vertexai.generative_models import GenerativeModel, ChatSession
+
+import logging
+from google.cloud import logging as gcp_logging
+
+# ------  Below cloud logging code is for Qwiklab's internal use, do not edit/remove it. --------
+# Initialize GCP logging
+gcp_logging_client = gcp_logging.Client()
+gcp_logging_client.setup_logging()
+
+project_id = "qwiklabs-gcp-03-ab602ac48c07"
+location = "us-central1"
+
+vertexai.init(project=project_id, location=location)
+model = GenerativeModel("gemini-1.0-pro")
+chat = model.start_chat()
+
+def get_chat_response(chat: ChatSession, prompt: str) -> str:
+    logging.info(f'Sending prompt: {prompt}')
+    response = chat.send_message(prompt)
+    logging.info(f'Received response: {response.text}')
+    return response.text
+
+prompt = "Hello."
+print(get_chat_response(chat, prompt))
+
+prompt = "What are all the colors in a rainbow?"
+print(get_chat_response(chat, prompt))
+
+prompt = "Why does it appear when it rains?"
+print(get_chat_response(chat, prompt))
